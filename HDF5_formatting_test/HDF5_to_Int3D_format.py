c83a7aef76d6a4ee6aabfbc05cd189844c19fc57
queenofthefairies <siobhan.physics@gmail.com>
2026-05-28 15:43:12 +1000

    finished efficiency calibration and normalisation by BM1 counts, summing frames and concatenating HDF. now to test on wombat

--- HDF5_formatting_test/HDF5_to_Int3D_format.py
+++ HDF5_formatting_test/HDF5_to_Int3D_format.py
@@ -10,7 +10,6 @@
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
-import csv
 import shutil
 
 import SingleCrystalHDF
@@ -20,12 +19,59 @@ formatted_data_dir = 'formatted_data'
 first_file_number = 107754
 last_file_number = 107755
 
-files_list = []
-for n in range(first_file_number,last_file_number+1):
-    file_name = 'WBT0{0}.nx.hdf'.format(n)
-    files_list.append(file_name)
+temperature = 40
+tilt = 0
+omega = -9
 
-files_list = ['WBT0107754.nx.hdf', 'WBT0107772.nx.hdf']
+if temperature == 40:
+    if tilt == 0:
+        if omega == -9:
+            BM1_normalisation_value = 58000
+            run_number_list = [[107754, 107772],
+                               [107755, 107773],
+                               [107756, 107774],
+                               [107757, 107775]]
+
+############# first sum frames from different run numbers if required
+file_list = []
+for i in range(len(run_number_list)):
+    sector_file = run_number_list[i]
+    if type(run_number_list[i]) == list:
+        print('summing frames in files')
+        files_to_sum_list = []
+        for run_number in run_number_list[i]:
+            file_to_sum_name = '{0}/WBT0{1}.nx.hdf'.format(data_dir,run_number)
+            files_to_sum_list.append(file_to_sum_name)
+        output_file = 'summed_{0}_{1}.nx.hdf'.format(run_number_list[i][0],
+                                                     run_number_list[i][-1])
+        SingleCrystalHDF.sum_equivalent_frames_in_HDFs(files_to_sum_list, output_file)
+    elif type(run_number_list[i]) == int:
+        new_sector_file = '{0}/WBT0{1}.nx.hdf'.format(data_dir,run_number_list[i])
+        file_list.append(new_sector_file)
+
+############# then concatenate files
+output_file = 'concatenated_{0}mK_tilt{1}_omega{2}.nx.hdf'.format(temperature,
+                                                                  tilt,
+                                                                  omega)
+
+output_file_path = data_dir +'/' + output_file
+SingleCrystalHDF.concatenate_HDF(file_list, output_file_path)
+
+############# then re-format for int3D
+SingleCrystalHDF.HDF_to_Int3D_format(data_dir, [output_file], formatted_data_dir,
+                                     eom_angle = 'som', echi_angle = 0, 
+                                     ephi_angle = 'msom', 
+                                     normalisation_value = BM1_normalisation_value,
+                                     efficiency_calibration='eff_2026_04_13.gumtree.hdf') 
+
+print()
+print('      Done aggregating dataset for {0} mK, tilt {1}, som {2}'.format(temperature,tilt,omega))
+# files_list = []
+# for n in range(first_file_number,last_file_number+1):
+#     file_name = 'WBT0{0}.nx.hdf'.format(n)
+#     files_list.append(file_name)
+
+# files_list = ['WBT0107754.nx.hdf', 'WBT0107772.nx.hdf']
 # SingleCrystalHDF.HDF_to_Int3D_format(data_dir, files_list, formatted_data_dir,
 #                                     eom_angle = 'som', echi_angle = 0, 
 #                                     ephi_angle = 'msom', 
@@ -42,12 +88,12 @@ files_list = ['WBT0107754.nx.hdf', 'WBT0107772.nx.hdf']
 # SingleCrystalHDF.concatenate_HDF(concatenate_file_list, output_file)
 
 ##################### SUMMING EQUIVALENT DETECTOR FRAMES IN FILES
-input_files = ['data/WBT0107754.nx.hdf', 'data/WBT0107772.nx.hdf']
-output_file = 'summed.nx.hdf'
-#SingleCrystalHDF.sum_equivalent_frames_in_HDFs(input_files, output_file)
-data_dir = ''
-SingleCrystalHDF.HDF_to_Int3D_format(data_dir, ['summed.nx.hdf'], formatted_data_dir,
-                                     eom_angle = 'som', echi_angle = 0, 
-                                     ephi_angle = 'msom', 
-                                     normalisation_value = 0,
-                                     efficiency_calibration='eff_2026_04_13.gumtree.hdf') 
+# input_files = ['data/WBT0107754.nx.hdf', 'data/WBT0107772.nx.hdf']
+# output_file = 'summed.nx.hdf'
+# #SingleCrystalHDF.sum_equivalent_frames_in_HDFs(input_files, output_file)
+# data_dir = ''
+# SingleCrystalHDF.HDF_to_Int3D_format(data_dir, ['summed.nx.hdf'], formatted_data_dir,
+#                                      eom_angle = 'som', echi_angle = 0, 
+#                                      ephi_angle = 'msom', 
+#                                      normalisation_value = 0)#,
+                                     #efficiency_calibration='eff_2026_04_13.gumtree.hdf') 
--- HDF5_formatting_test/SingleCrystalHDF.py
+++ HDF5_formatting_test/SingleCrystalHDF.py
@@ -1,7 +1,6 @@
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
-import csv
 import shutil
 
 ################################################################################
